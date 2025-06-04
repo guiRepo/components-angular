e90328aa4c71b480c40ba1e342f43a8ee94a384a
Guilherme André de Melo <guilhermeanm95@hotmail.com>
2025-06-04 01:36:05 -0300

    Conclusão do layout do componente e comportamento

--- src/components/switch/switch.component.ts
+++ src/components/switch/switch.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+
+
 
 @Component({
   selector: 'switch-component',
-  imports: [],
+  imports: [FormsModule, MatIconModule],
   templateUrl: './switch.component.html',
   styleUrl: './switch.component.css'
 })
 export class SwitchComponent {
+  checked: boolean = true;
+  disabled: boolean = true;
 }
